15453a60544d7f0de7bd2eefe7a8e695554ae54d
pdro1812 <pdro181205@gmail.com>
2025-05-25 18:10:57 -0300

    selection

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
+import "fmt"
 
 func main() {
-	arr := size(3, 3)
+	arr := []int{23, 1, 10, 5, 2}
 	for _, v := range arr {
 		println(v)
 	}
+
+	duracao, comp, mov := selection(arr)
+
+    fmt.Printf("Tempo de execução: %v\n", duracao)
+    fmt.Printf("comparações: %d, movimentacoes: %d\n", comp, mov)
+
 }
--- selection.go
+++ selection.go
@@ -1,2 +1,29 @@
 package main
 
+import "time"
+
+func selection (arr[]int)(time.Duration, int, int){
+	inicio:= time.Now()
+
+	mov := 0
+	comp := 0
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		chave := i
+		for j := i + 1; j < n; j++ {
+			comp++
+			if arr[j] < arr[chave] {
+				chave = j
+			}
+		}
+		if chave != i {
+			arr[i], arr[chave] = arr[chave], arr[i]
+			mov++
+		}
+	}
+	duracao := time.Since(inicio)
+	return duracao, comp, mov
+
+
+}
